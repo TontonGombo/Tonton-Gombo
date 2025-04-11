6686f38ed3da74aac13f58ce2483fa60d16a1833
TontonGombo <tontongombo237@gmail.com>
2025-04-11 06:51:35 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  author: 'Meppe',
+  botName: "Tonton Gombo Bot",
+  botOwner: 'Tonton Gombo', // Your name
+  ownerNumber: '237696574660', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
